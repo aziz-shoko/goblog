c6a74e5783d22143f4918969217bf2a3591c3292
aziz-shoko <azizbek.shokosimov@gmail.com>
2025-06-29 18:54:15 -0500

    add get handlers to main

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/aziz-shoko/goblog/internal/handler"
+	"github.com/aziz-shoko/goblog/internal/service"
+	"github.com/aziz-shoko/goblog/internal/store"
+)
+
+func main() {
+	postStore := store.NewInMemoryStore()
+	postService := service.NewPostService(postStore)
+	postHandler := handler.NewPostHandler(postService)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /posts", handler.LoggingMiddleware(postHandler.CreatePost))
+	mux.HandleFunc("GET /post/{id}", handler.LoggingMiddleware(postHandler.GetPostByID))
+	mux.HandleFunc("GET /posts", handler.LoggingMiddleware(postHandler.GetPostsAll))
+
+	log.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
